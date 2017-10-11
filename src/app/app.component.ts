3913d90bc20a4eb66ae3f0f2f2b45822a464ff1f
Nicholas Duntze <NDuntze@mro.com>
2017-10-11 15:43:57 -0400

    added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,14 @@
-import { Component } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core';
+import { Component } from '@angular/core'
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService],
+@Component ({
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <a routerLink="/heroes">Heroes</a>
+        <router-outlet></router-outlet>
+    `,
 })
 
-export class AppComponent implements OnInit {
-  title = 'My First Angular App';
-  selectedHero: Hero;
-  heroes: Hero[];
-  constructor(private heroService: HeroService) { }
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-  onSelect(hero: Hero): void {
-    if (this.selectedHero === hero) {
-      this.selectedHero = undefined;
-    } else {
-      this.selectedHero = hero;
-    }
-  }
-  alert(): void {
-    alert(`Pshh... You're my hero`);
-  }
+export class AppComponent {
+    title = 'My First Angular App';
 }
